b228edcbff83c540f704079d458fb3d010833d3b
masato.kinoshita <mst.supertramp99@gmail.com>
2020-10-14 08:46:23 +0000

    first commit kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,57 @@
+class TasksController < ApplicationController
+    before_action :task_set, only: [:show, :edit, :update, :destroy]
+    
+    def index
+        @tasks = Task.all
+    end
+    
+    def show
+        task_set
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = Task.new(strong_params)
+        if @task.save
+            flash[:success] = "タスクが作成されました。"
+            redirect_to @task
+        else
+            flash.now[:danger] = "タスクが作成されませんでした。"
+            render new
+        end
+    end
+    
+    def edit
+        task_set
+    end
+    
+    def update
+        task_set
+        if @task.update(strong_params)
+            flash[:success] = "タスクは正常に変更されました。"
+            redirect_to @task
+        else
+            flash[:danger] = "タスクは変更されませんでした。"
+            render :edit
+        end
+    end
+    
+    def destroy
+        task_set
+        @task.destroy
+        flash[:success] = "タスクは削除されました"
+        redirect_to tasks_url
+    end
+    
+    private
+        def task_set
+            @task = Task.find(params[:id])
+        end
+    
+        def strong_params
+            params.require(:task).permit(:content)
+        end
+end
